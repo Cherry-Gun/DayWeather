802d33c5972a3f3ccd87df9ef60741ede82c6f1a
jason <jason-white@live.cn>
2016-03-13 00:16:15 +0800

    显示具体地理位置的天气信息。

--- app/src/main/java/com/wyb/dayweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/wyb/dayweather/activity/ChooseAreaActivity.java
@@ -2,14 +2,19 @@ package com.wyb.dayweather.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.wyb.dayweather.R;
 import com.wyb.dayweather.db.DayWeatherDB;
 import com.wyb.dayweather.model.City;
@@ -18,9 +23,9 @@ import com.wyb.dayweather.model.Province;
 import com.wyb.dayweather.util.HttpCallbackListener;
 import com.wyb.dayweather.util.HttpUtil;
 import com.wyb.dayweather.util.Utility;
+
 import java.util.ArrayList;
 import java.util.List;
-
 
 
 public class ChooseAreaActivity extends Activity {
@@ -47,6 +52,13 @@ public class ChooseAreaActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean("city_selected", false)) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
         listView = (ListView) findViewById(R.id.list_view);
         titleText = (TextView) findViewById(R.id.title_text);
@@ -62,6 +74,12 @@ public class ChooseAreaActivity extends Activity {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(index);
                     queryCounties();
+                } else if (currentLevel == LEVEL_COUNTY) {
+                    String countyCode = countyList.get(index).getCountyCode();
+                    Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+                    intent.putExtra("county_code", countyCode);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
